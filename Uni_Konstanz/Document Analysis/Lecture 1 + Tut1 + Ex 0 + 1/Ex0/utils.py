803fce3461f1f56cbb9b90ea48207f572316613b
Aleksandr Belous <alexander.it.jobs@gmail.com>
2025-09-06 21:01:10 +0200

    started ex01

--- Uni_Konstanz/Document Analysis/Lecture 1 + Tut1 + Ex 0 + 1/Ex0/utils.py
+++ Uni_Konstanz/Document Analysis/Lecture 1 + Tut1 + Ex 0 + 1/Ex0/utils.py
@@ -9,15 +9,33 @@ __copyright__   = "Copyright 2020, DBVIS"
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+import os
+import xml.etree.ElementTree as ET
+import pandas as pd
 
 FILEPATH_LOREM_IPSUM = 'data/lorem_ipsum.txt'
-
+FILEPATH_DEBATES_XML = 'data/debates/'
+FILEPATH_TV_CSV = 'data/tv/'
 
 def read_lorem_ipsum_text():
     """Read in lorem ipsum text"""
     f = open(FILEPATH_LOREM_IPSUM, 'r');
     content = f.read()
     return content
+
+def read_xml():
+    """Read in xml files"""
+    for i in os.listdir(FILEPATH_DEBATES_XML):
+        tree = ET.parse(FILEPATH_DEBATES_XML + i)
+        root = tree.getroot()
+        return ET.dump(root)
+
+# //TODO
+def read_scv():
+    for i in os.listdir(FILEPATH_TV_CSV):
+        file = pd.read_csv(FILEPATH_TV_CSV + i)
+        
+    return file
     
 def create_word_cloud(wordFrequencies, title):
     print(title + ':')
@@ -31,3 +49,6 @@ def create_word_cloud(wordFrequencies, title):
     plt.axis("off")
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.show()
+
+
+print(read_scv())
