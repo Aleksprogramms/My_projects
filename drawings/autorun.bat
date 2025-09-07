63f0136cba339913ba408c21d1da64423509d4fa
Aleksandr Belous <alexander.it.jobs@gmail.com>
2025-09-07 18:41:48 +0200

    Lecture 1

--- drawings/autorun.bat
+++ drawings/autorun.bat
@@ -1,4 +1,4 @@
 @echo off
 mode con: cols=120 lines=40
-python C:\Users\bagim\Desktop\projects\projects\drawings\skull\skull.py
+python C:\Users\bagim\Desktop\My_projects\drawings\skull\skull.py
 pause
